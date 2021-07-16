dde6df2ff7f2cc98e191e0cced427e0eb47aeba8
Henry Post <henryfbp@gmail.com>
2021-07-16 15:04:36 -0500

    add commentbox structure

--- react-apps-in-dev-and-prod/comment-app/src/App.js
+++ react-apps-in-dev-and-prod/comment-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CommentList from './CommentList';
+import CommentBox from './CommentBox';
 
 class App extends React.Component {
   render(){
     return (
       <div>
+        <CommentBox/>
         <CommentList/>
       </div>
     );
--- react-apps-in-dev-and-prod/comment-app/src/CommentList.js
+++ react-apps-in-dev-and-prod/comment-app/src/CommentList.js
@@ -6,6 +6,9 @@ class CommentList extends React.Component {
         return (
             <div className="commentList">
                 <Comment key={1}/>
+                <Comment key={2}/>
+                <Comment key={3}/>
+                <Comment key={4}/>
             </div>
         )
     }
