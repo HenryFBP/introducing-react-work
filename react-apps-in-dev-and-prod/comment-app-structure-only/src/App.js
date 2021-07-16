5d94da9babd262dcf5074bb25714d412d27d30ce
Henry Post <henryfbp@gmail.com>
2021-07-16 12:47:04 -0500

    make snapshot of structure only comment-app

---
+++ react-apps-in-dev-and-prod/comment-app-structure-only/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CommentList from './CommentList';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <CommentList/>
+      </div>
+    );
+  }
+}
+  
+export default App;
