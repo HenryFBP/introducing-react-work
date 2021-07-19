b7b0ef44b4e1b481ea1b90d38f0134c7578420f6
Henry Post <henryfbp@gmail.com>
2021-07-19 15:25:50 -0500

    add props examples

---
+++ props-and-state/Props/IntroducingProps/Props.js
@@ -0,0 +1,14 @@
+class Greeting extends React.Component {
+    render() {
+        return (
+            <div className='prop-class'>
+                <h2>{this.props.message}</h2>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Greeting message="Hello React devs!" />,
+    document.getElementById('react-prop1')
+);
