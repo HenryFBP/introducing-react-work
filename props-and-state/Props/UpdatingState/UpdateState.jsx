a3ffbcdceaf9e5a4caf8109c02423b1c51e597bf
Henry Post <henryfbp@gmail.com>
2021-07-20 16:37:39 -0500

    start updatestate

---
+++ props-and-state/Props/UpdatingState/UpdateState.jsx
@@ -0,0 +1,79 @@
+class DetailsBox extends React.Component {
+    state = {
+        name: 'John',
+        color: 'green',
+        backgroundColor: 'pink',
+        subject: 'Psychology'
+    };
+
+    //using arrow notation automatically binds `pickRandomName` to `this`...
+    //use arrow notation if you wish to bind a function to the class instance 
+    pickRandomName = () => {
+        var names = [
+            'Alice',
+            'Bob',
+            'Carol',
+            'Dora',
+            'Elisa'
+        ];
+
+        var nameidx = Math.floor(Math.random() * names.length);
+
+        // this is a special function that MUST be used to update state.
+        // it ensures the component re-renders normally.
+        this.setState({
+            name: names[nameidx]
+            //only pass name as we only want to update the name.
+        }); //react will merge this field with the original 'state' object. 
+        // React merges partial state changes.
+    }
+
+    pickRandomColor = () => {
+        var colors = [
+            'blue',
+            'magenta',
+            'lavender',
+            'violet',
+            'red'
+        ];
+
+        var colorIdx = Math.floor(Math.random() * colors.length);
+
+        this.setState({
+            color: colors[colorIdx]
+        });
+    }
+
+    pickRandomBackgroundColor = () => {
+        var colors = [
+            'lime',
+            'skyblue',
+            'orange',
+            'yellow',
+            'purple'
+        ];
+
+        var colorIdx = Math.floor(Math.random() * colors.length);
+
+        this.setState({
+            backgroundColor: colors[colorIdx]
+        });
+    }
+
+    pickRandomSubject = () => {
+        var subjects = [
+            'Math',
+            'Science',
+            'Biochem',
+            'Physics',
+            'Psychology',
+            'History'
+        ];
+
+        var subjectIdx = Math.floor(Math.random() * subjects.length);
+
+        this.setState({
+            subject: subjects[subjectIdx]
+        });
+    }
+}
