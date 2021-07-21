5c195b3be2f21b825eb93630919e8f16c7f2b5e6
Henry Post <henryfbp@gmail.com>
2021-07-21 13:04:41 -0500

    add lists

---
+++ advanced-features-in-react/ListsAndKeys/Lists/Lists.jsx
@@ -0,0 +1,16 @@
+class NamesList extends React.Component {
+    render() {
+        var listItems = this.props.names.map(
+            (name) => <li>{name.toUpperCase()}</li> //should have a key property, this is not best practices...
+        );
+
+        return (
+            <ul className="names"> {listItems} </ul>
+        )
+    }
+}
+
+const names = 'Esther Lily Candace Edward Sidney Henry'.split(' ');
+
+ReactDOM.render(<NamesList names={names} />,
+    document.getElementById('react-lists'));
