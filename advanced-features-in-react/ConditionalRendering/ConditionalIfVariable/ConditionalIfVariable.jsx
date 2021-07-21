84afdf0de4687442e95595522c744d810ebeec2c
Henry Post <henryfbp@gmail.com>
2021-07-21 15:07:05 -0500

    add ConditionalIfVariable

---
+++ advanced-features-in-react/ConditionalRendering/ConditionalIfVariable/ConditionalIfVariable.jsx
@@ -0,0 +1,71 @@
+function Circle(props) {
+    const circleStyle = {
+        backgroundColor: props.color
+    };
+
+    return (
+        <div className="circle"
+            style={circleStyle}></div>
+    );
+}
+
+function GreenCircle(props) {
+    return <Circle color="green"></Circle>
+}
+
+function RedCircle(props) {
+    return <Circle color="red"></Circle>
+}
+
+function StopButton(props) {
+    return (
+        <button className="button" onClick={props.toggle}>
+            Stop
+        </button>
+    );
+}
+
+function GoButton(props) {
+    return (
+        <button className="button" onClick={props.toggle}>
+            Go
+        </button>
+    );
+}
+
+//stateful component with 1 state
+class ToggleCircle extends React.Component {
+    state = {
+        go: true
+    }
+
+    toggle = () => {
+        this.setState(function (prevState) {
+            return {
+                go: !prevState.go
+            } //inversion of my previous state
+        });
+    }
+
+    render() {
+        let button, circle;
+
+        if (this.state.go) {
+            circle = <GreenCircle />
+            button = <StopButton toggle={this.toggle} />
+        } else {
+            circle = <RedCircle />
+            button = <GoButton toggle={this.toggle} />
+        }
+
+        return (
+            <div>
+                {circle}
+                {button}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<ToggleCircle />,
+    document.getElementById('react-conditional-if'));
